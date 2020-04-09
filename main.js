112823757cb0a89840d2f30d7c37fbc16370172d
Charles Baker <bakercharles14@gmail.com>
2020-04-09 14:04:22 -0400

    created buttons in HTML

---
+++ main.js
@@ -0,0 +1,15 @@
+// for (let i = 1; i < 6; i++) {
+//     const questions = document.createElement('div')
+//         questions.id = 'question-1-' + i
+//         questions.className = 'category-1'
+// }
+// const q1c1 = document.div.question-1-1
+// q1c1.innerHTML = '10'
+
+let question = ''
+const openQuestion = () => {
+    question = document.getElementByClassName('category-1').getElementByClassName('question-1')
+    question.addEventListener('click', () => {
+        
+    })
+}
