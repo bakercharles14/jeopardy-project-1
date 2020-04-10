b650266cf9b0c5870704fe6dd8d5ffaf4619892e
Charles Baker <bakercharles14@gmail.com>
2020-04-10 10:23:47 -0400

    changed logo/added questionbank object array

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
+const questionBank = [
+    {
+        q: 'What is this class?',
+        answers: ['SEI27', 'SEI9', 'IDK', "Brandon's boys"],
+        correctAnswer: 'SEI27',
+        points: 10 },
+    
+]
 //Function that creates all divs that can be clicked to answer questions
-function addTenQuestions() {
-for (let i = 0; i < 5; i++) {
+const addTenPointQuestions = () => {
+for (let i = 0; i < 1; i++) {
     let questions = document.createElement('div')
-        questions.classList.add('tenQuestions')
-        document.querySelector('body').appendChild(questions)
-        // questions.innerHTML = '10'
-        questions.addEventListener('click', () => {
-            
+    questions.classList.add('tenQuestions')
+    document.querySelector('body').appendChild(questions)
+    // questions.innerHTML = '10'
+    questions.addEventListener('click', () => {
+        let questionsDisplay = document.createElement('div')
+        document.querySelector('body').appendChild(questionsDisplay)
+        questionsDisplay.innerHTML = questionBank[0].q
         })
 }
 }
-addTenQuestions();
+addTenPointQuestions();
 
-// const questionBank = [
-//     {
-//         q: 'What is this class?',
-//         answers: ['SEI27', 'SEI9', 'IDK', "Brandon's boys"],
-//         correctAnswer: 'SEI27',
-//         points: 10 },
-    
-// ]
+
 
 // let questionButton = ''
 // const openQuestion = () => {
