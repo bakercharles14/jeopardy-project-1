9ec331040ff865c4f69b7cb0f13dae75636f2cd4
Charles Baker <bakercharles14@gmail.com>
2020-04-10 13:56:51 -0400

    added event listener to answers, added scoreboard

--- main.js
+++ main.js
@@ -1,33 +1,53 @@
+//Bank that contains all questions
 const questionBank = [
     {
-        q: 'What is this class?',
-        answers: ['SEI27', 'SEI9', 'IDK', "Brandon's boys"],
-        correctAnswer: 'SEI27',
-        points: 10 },
-    
+        q: 'What is the name of the final course in all "Mario Kart" videogames?',
+        answers: ['Coconut Mall', "Bowser's Castle", 'Rainbow Road', 'Moo Moo Meadows'],
+        correctAnswer: 'Rainbow Road',
+        points: 10,
+        category: 'video-game-history'
+    },
+    {
+        q: 'The classic Atari 2600 game controller has how many buttons?',
+        answers: ['1', '3', '4', "7"],
+        correctAnswer: '1',
+        points: 30,
+        category: 'video-game-history'
+    },
+
 ]
 //Function that creates all divs that can be clicked to answer questions
 const addTenPointQuestions = () => {
-for (let i = 0; i < 1; i++) {
-    let questions = document.createElement('div')
-    questions.classList.add('tenQuestions')
-    document.querySelector('body').appendChild(questions)
-    // questions.innerHTML = '10'
-    questions.addEventListener('click', () => {
-        let questionsDisplay = document.createElement('div')
-        document.querySelector('body').appendChild(questionsDisplay)
-        questionsDisplay.innerHTML = questionBank[0].q
+    //for loop that creates the divs
+    for (let i = 0; i < 3; i++) {
+        let questions = document.createElement('div')
+        questions.classList.add('tenQuestions')
+        document.querySelector('.jeopardyBoard').appendChild(questions)
+        // questions.innerHTML = '10'
+        //event listener that will create dive to have questions reside in
+        questions.addEventListener('click', () => {
+            let questionsDisplay = document.createElement('div')
+            document.querySelector('footer').appendChild(questionsDisplay)
+            questionsDisplay.innerHTML = questionBank[i].q
+            //for loop to populate the answers in another div under questions
+            for (let j = 0; j < questionBank[i].answers.length; j++) {
+                let answersDisplay = document.createElement('div')
+                document.querySelector('footer').appendChild(answersDisplay)
+                answersDisplay.innerHTML = questionBank[i].answers[j]
+                answersDisplay.addEventListener('click', () => {
+                    // console.log(questionBank[i].answers[j])
+                    if (questionBank[i].answers[j] === questionBank[i].correctAnswer) {
+                        console.log('You get ' + questionBank[i].points +' Points')
+                        let score = questionBank[i].points
+                        let totalScore = document.getElementById('score').innerHTML
+                        score += totalScore
+                        alert('Ding Ding Ding!')
+                    } else {
+                        
+                    }
+                })
+            }
         })
-}
+    }
 }
 addTenPointQuestions();
-
-
-
-// let questionButton = ''
-// const openQuestion = () => {
-//     questionButton = document.getElementByClassName('category-1').getElementByClassName('question-1')
-//     questionButton.addEventListener('click', () => {
-
-//     })
-// }
