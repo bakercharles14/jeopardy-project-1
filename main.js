dbc8e2605b23bfab92e19474e69b674228abcbc6
Charles Baker <bakercharles14@gmail.com>
2020-04-13 20:26:02 -0400

    changes to specify category title

--- main.js
+++ main.js
@@ -6,69 +6,79 @@ const questionBank = [
         correctAnswer: 'Magnavox Odyssey',
         points: 10,
         category: 'Video Game History',
+        catNum: 1
     },
     {
         q: 'Which videogame holds the record for having the highest budget ever to produce?',
         answers: ['The Witcher 3: Wild Hunt', 'Destiny', 'GTA V', 'COD: MW2'],
         correctAnswer: 'Destiny',
         points: 30,
-        category: 'Video Game History'
+        category: 'Video Game History',
+        catNum: 1
     },
     {
         q: 'What is the name of the first-ever video game?',
         answers: ['Tennis for Two', 'Pong', 'Spacewar!', 'Periscope'],
         correctAnswer: 'Tennis for Two',
         points: 50,
-        category: 'Video Game History'
+        category: 'Video Game History',
+        catNum: 1
     },
     {
         q: 'What is the highest level a player can reach in "Pac-Man"?',
         answers: ['180', '256', '225', '300'],
         correctAnswer: '256',
         points: 80,
-        category: 'Video Game History'
+        category: 'Video Game History',
+        catNum: 1
     },
     {
         q: 'The classic Atari 2600 game controller has how many buttons?',
         answers: ['3', '1', '4', '0'],
         correctAnswer: '1',
         points: 100,
-        category: 'Video Game History'
+        category: 'Video Game History',
+        catNum: 1
     },
     {
         q: 'What Nintendo console was the successor to the NES?',
         answers: ['Nintendo 64', 'SNES', 'Game Boy Advance', 'Game Cube'],
         correctAnswer: 'SNES',
         points: 10,
-        category: 'Video Game Systems'
+        category: 'Video Game Systems',
+        catNum: 2
     },
     {
         q: 'The Nintendo Game Boy utilized which type of display system?',
         answers: ['CRT', 'OLED', 'LED', 'LCD'],
         correctAnswer: 'LCD',
         points: 30,
-        category: 'Video Game Systems'
+        category: 'Video Game Systems',
+        catNum: 2
     },
     {
         q: 'The PlayStation was first designed as a companion to this gaming console.',
         answers: ['SNES', 'Sony Dreamcast', 'Game Cube', 'Sega Genesis'],
         correctAnswer: 'SNES',
         points: 50,
-        category: 'Video Game Systems'
+        category: 'Video Game Systems',
+        catNum: 2
     },
     {
         q: 'How many games were out for the PlayStation when it was first released?',
         answers: ['30', '14', '6', '17'],
         correctAnswer: '17',
         points: 80,
-        category: 'Video Game Systems'
+        category: 'Video Game Systems',
+        catNum: 2
     },
     {
         q: 'What was the first videogame console with a CD-ROM embedded into it?',
         answers: ['Panasonic 3DO', 'SNES', 'Genesis', 'PSOne'],
         correctAnswer: 'Panasonic 3DO',
         points: 100,
-        category: 'Video Game Systems'
+        category: 'Video Game Systems',
+        catNum: 2
     },
     //category 3: characters
     {
@@ -76,35 +86,40 @@ const questionBank = [
         answers: ['Resident Evil', 'Metal Gear', 'Mass Effect', "Uncharted"],
         correctAnswer: 'Metal Gear',
         points: 10,
-        category: 'Characters'
+        category: 'Characters',
+        catNum: 3
     },
     {
         q: 'Which "Mortal Kombat" character has the ability to transform into a dragon?',
         answers: ['Liu Kang', 'SNES', 'Genesis', "PSOne"],
         correctAnswer: 'Liu Kang',
         points: 30,
-        category: 'Characters'
+        category: 'Characters',
+        catNum: 3
     },
     {
         q: 'Which Mario character has appeared in all but one "Mario Party" game?',
         answers: ['Luigi', 'Yoshi', 'Pricess Peach', 'Wario'],
         correctAnswer: 'Wario',
         points: 50,
-        category: 'Characters'
+        category: 'Characters',
+        catNum: 3
     },
     {
         q: 'What is the name of the StarCraft character who sacrificed himself to kill the Overmind?',
         answers: ['Artanis', 'Tassadar', 'Aldaris', 'Zeratul'],
         correctAnswer: 'Tassadar',
         points: 80,
-        category: 'Characters'
+        category: 'Characters',
+        catNum: 3
     },
     {
         q: `What type of animal is Sonic's companion "Knuckles"?`,
         answers: ['Platypus', 'Hedgehog', 'Echidna', "Anteater"],
         correctAnswer: 'Echidna',
         points: 100,
-        category: 'Characters'
+        category: 'Characters',
+        catNum: 3
     },
 
     //category 4: Name of the game
@@ -113,35 +128,40 @@ const questionBank = [
         answers: ['Mario Kart', 'Mario Racing', 'Mario Party', 'Mario Cart'],
         correctAnswer: 'Mario Kart',
         points: 10,
-        category: 'Name of the Game'
+        category: 'Name of the Game',
+        catNum: 4
     },
     {
         q: 'Mario originated as a character in which classic video game?',
         answers: ['Super Mario Brothers', 'Donkey Kong', 'Mario Bros', 'Mario'],
         correctAnswer: 'Donkey Kong',
         points: 30,
-        category: 'Name of the Game'
+        category: 'Name of the Game',
+        catNum: 4
     },
     {
         q: 'Which videogame is generally considered to be the original first-person shooter?',
         answers: ['Wayout', 'Wolfenstein 3D', 'Maze War', 'Akalabeth: World of Doom '],
         correctAnswer: 'Maze War',
         points: 50,
-        category: 'Name of the Game'
+        category: 'Name of the Game',
+        catNum: 4
     },
     {
         q: 'Ed Boon and John Tobias are the original designers of which renowned videogame franchise?',
         answers: ['Minecraft', 'Resident Evil', 'Mortal Kombat', 'Street Fighter'],
         correctAnswer: 'Liu Kang',
         points: 80,
-        category: 'Name of the Game'
+        category: 'Name of the Game',
+        catNum: 4
     },
     {
         q: 'What was the name of the last game released as a physical cartridge for the Super Nintendo?',
         answers: ['Frogger', "Timon & Pumbaa's Jungle Games", "Metal Slader Glory: Director's Cut", 'Fire Emblem: Thracia 776'],
         correctAnswer: 'Fire Emblem: Thracia 776',
         points: 100,
-        category: 'Name of the Game'
+        category: 'Name of the Game',
+        catNum: 4
     },
     //category 5: fame in the game
     {
@@ -149,35 +169,40 @@ const questionBank = [
         answers: ['Beat It', 'Billie Jean', 'Thriller', 'Moonwalker'],
         correctAnswer: 'Moonwalker',
         points: 10,
-        category: 'Fame in the Game'
+        category: 'Fame in the Game',
+        catNum: 5
     },
     {
         q: 'The most-popular American football videogame franchise is named after which individual?',
         answers: ['John Madden', 'John Gruden', 'Vince Lombardi', 'Bill Walsh'],
         correctAnswer: 'John Madden',
         points: 30,
-        category: 'Fame in the Game'
+        category: 'Fame in the Game',
+        catNum: 5
     },
     {
         q: 'Which character in the "Street Fighter" series was inspired by Bruce Lee?',
         answers: ['Ryu', 'Gouken', 'Fei Long', 'Guy'],
         correctAnswer: 'Fei Long',
         points: 50,
-        category: 'Fame in the Game'
+        category: 'Fame in the Game',
+        catNum: 5
     },
     {
         q: 'The protagonist in "Grand Theft Auto: Vice City" was voiced by which actor?',
         answers: ['Ray Liotta', 'Robert De Niro', 'Paul Sorvino', 'Joe Pesci'],
         correctAnswer: 'Ray Liotta',
         points: 80,
-        category: 'Fame in the Game'
+        category: 'Fame in the Game',
+        catNum: 5
     },
     {
         q: `Which iconic actor provided the voice of the titular character during the 1990's "Sonic the Hedgehog" cartoon?`,
         answers: ['Jaleel White', 'Tom Hanks', 'David Schwimmer', 'Alfonso Ribeiro'],
         correctAnswer: 'Jaleel White',
         points: 100,
-        category: 'Fame in the Game'
+        category: 'Fame in the Game',
+        catNum: 5
     },
 ]
 const addCategoryTitles = () => {
@@ -185,7 +210,7 @@ const addCategoryTitles = () => {
         let categoryTitles = document.createElement('div')
         categoryTitles.classList.add('categoryTitles')
         document.querySelector('.categoryTitleBoard').appendChild(categoryTitles)
-        categoryTitles.innerHTML = questionBank[0].category
+        categoryTitles.innerHTML = questionBank[i].category
     }
 }
 addCategoryTitles()
@@ -246,6 +271,10 @@ const addJeopardyBoard = () => {
     }
 }
 addJeopardyBoard();
-if (questionBank.category === 'Vide Game History') {
+if (questionBank.category === 'Video Game History') {
+
+}
+const winOrLose = () => {
     
 }
+winOrLose ()
