064d7ef23c19b4cb97ded7703401e302db688118
Charles Baker <bakercharles14@gmail.com>
2020-04-14 23:34:25 -0400

    commented out sound/off func until fixed

--- main.js
+++ main.js
@@ -242,14 +242,16 @@ const addJeopardyBoard = () => {
                     answersDisplay.addEventListener('click', () => {
                         let score = questionBank[i].points
                         if (questionBank[i].answers[j] === questionBank[i].correctAnswer) {
-                            let myCorrectSound = document.getElementById('correct-sound')
-                            myCorrectSound.play()
+                            //commented out until off function complete
+                            // let myCorrectSound = document.getElementById('correct-sound')
+                            // myCorrectSound.play()
                             console.log('You get ' + questionBank[i].points + ' points')
                             totalScore += score
                             alert('Correct! You get ' + questionBank[i].points + ' points.' + ' Explanation: ' + questionBank[i].explanation)
                         } else if (questionBank[i].answers[j] !== questionBank[i].correctAnswer) {
-                            let myWrongSound = document.getElementById('wrong-sound')
-                            myWrongSound.play()
+                            //commented out until off function complete
+                            // let myWrongSound = document.getElementById('wrong-sound')
+                            // myWrongSound.play()
                             totalScore -= score
                             console.log('Wrong answer! You lose ' + questionBank[i].points + ' points!')
                             alert('Wrong! You lose ' + questionBank[i].points + ' points.' + ' The correct answer was ' + questionBank[i].correctAnswer + '. ' + 'Explanation: ' +  questionBank[i].explanation)
